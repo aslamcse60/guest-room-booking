debca0420f5ae221ef2827ccd549a3851014293a
aslamcse60 <mohamedaslamacse@gmail.com>
2024-10-21 21:58:42 +0530

    first commit

---
+++ src/main/java/com/guestroom/GuestRoomBookingApplication.java
@@ -0,0 +1,13 @@
+package com.guestroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestRoomBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestRoomBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guestroom/GuestRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guestroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestRoomBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
